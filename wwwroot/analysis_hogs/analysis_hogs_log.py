593f8bd480c39bb40f6277a60b13621638f62860
zhaobing.lv <zhaobing.lv@tusen.ai>
2023-06-01 15:18:16 +0800

    fix hogs scripts bug.

--- wwwroot/analysis_hogs/analysis_hogs_log.py
+++ wwwroot/analysis_hogs/analysis_hogs_log.py
@@ -7,6 +7,8 @@ log_lines = []
 
 process_memory_samples = {}
 process_cpu_samples = {}
+memory_all_data={}
+cpu_all_data={}
 
 def sample_log_data(arr):
     result_arr = []
@@ -15,19 +17,39 @@ def sample_log_data(arr):
     if skip < 1:
         skip = 1
     while index < len(arr):
-        result_arr.append(arr[index])
+        result_arr.append([index,arr[index]])
         index+=skip
     return result_arr
-
 
 def process_log_data():
     global process_memory_samples
     global process_cpu_samples
+    global memory_all_data
+    global cpu_all_data
 
+    isFirstHeader = True
     for line in log_lines:
+        prefix_re=r'.*hogsParse.h\s[0-9]*\s[0-9]*'
+        line = re.sub(prefix_re,'',line)
+        reObj = re.match('\s*PID\s*NAME\s*MSEC\s*PIDS\s*SYS\s*MEMORY', line)
+        if reObj:
+            #print(line)
+            if not isFirstHeader:
+                memory_data_map=process_memory_data()
+                for process_name in memory_data_map:
+                    if process_name not in memory_all_data:
+                        memory_all_data[process_name] = []
+                    memory_all_data[process_name].append(memory_data_map[process_name])
+
+                cpu_data_map=process_cpu_data()
+                for process_name in cpu_data_map:
+                    if process_name not in cpu_all_data:
+                        cpu_all_data[process_name] = []
+                    cpu_all_data[process_name].append(cpu_data_map[process_name])
+            isFirstHeader = False
+
         reObj = re.match('\s+[0-9]+\s+.*%', line)
         if reObj:
-            print(line)
             reg=re.compile(r'\s+')
             line=reg.sub('|', line)
             str_array=line.split('|')[1:]
@@ -44,43 +66,31 @@ def process_log_data():
                 try:
                     process_memory_str=str_array[5].strip().replace('k', '')
                     process_memory=round(int(process_memory_str)/1024, 3)
-                    if process_id_str not in process_memory_samples:
-                        process_memory_samples[process_id_str] = []
-                    process_memory_samples[process_id_str].append([len(process_memory_samples[process_id_str]), process_memory])
+                    process_memory_samples[process_id_str]=process_memory
                 except:
-                    print('process_memory_str error')
+                    #print('warning: process_memory_str')
                     pass
 
                 try:
                     process_cpu_str=str_array[4].strip().replace('%', '')
                     process_cpu=round(float(process_cpu_str), 3)
-                    if process_id_str not in process_cpu_samples:
-                        process_cpu_samples[process_id_str] = []
-                    process_cpu_samples[process_id_str].append([len(process_cpu_samples[process_id_str]), process_cpu])
+                    process_cpu_samples[process_id_str]=process_cpu
                 except:
-                    print('process_cpu_str error')
+                    print('warning: process_cpu_str')
                     pass
-
-                
             except:
                 print('skip error line')
                 pass
 
-def process_charts_data():
+def process_memory_data():
     global process_memory_samples
-    global process_cpu_samples
 
-    process_memory_pie_data_array = []
     MAX_MEMORY_SIZE=30784
-    system_memory_sum = 0
     process_memory_map = {}
 
     for ch_key in process_memory_samples:
         process_name = ch_key[0:ch_key.rfind('-')]
-        process_memory_sum = 0
-        for sample_data in process_memory_samples[ch_key]:
-            process_memory_sum = process_memory_sum + sample_data[1]
-        process_memory_value = process_memory_sum / len(process_memory_samples[ch_key])
+        process_memory_value = process_memory_samples[ch_key]
 
         if process_name not in process_memory_map:
             process_memory_map[process_name] = process_memory_value
@@ -89,10 +99,46 @@ def process_charts_data():
     
     for process_name in process_memory_map:
         process_memory_value = process_memory_map[process_name]
-        system_memory_sum = system_memory_sum + process_memory_value
+        process_memory_map[process_name] = round(process_memory_value, 3)
+
+    process_memory_samples={}
+    return process_memory_map
+
+def process_cpu_data():
+    global process_cpu_samples
+    MAX_CPU=100
+    process_cpu_map = {}
+
+    for ch_key in process_cpu_samples:
+        process_name = ch_key[0:ch_key.rfind('-')]
+        process_cpu_value = process_cpu_samples[ch_key]
+        if process_name not in process_cpu_map:
+            process_cpu_map[process_name] = process_cpu_value
+        else:
+            process_cpu_map[process_name] = process_cpu_map[process_name] + process_cpu_value
+    
+    for process_name in process_cpu_map:
+        process_cpu_map[process_name] = round(process_cpu_map[process_name]/12,3)
+    
+    process_cpu_samples={}
+    return process_cpu_map
+
+
+def process_all_charts_data():
+    global memory_all_data
+    global cpu_all_data
+
+    process_memory_pie_data_array = []
+    MAX_MEMORY_SIZE=30784
+    system_memory_sum = 0
+    for process_name in memory_all_data:
+        process_memory_sum = 0
+        for sample_data in memory_all_data[process_name]:
+            process_memory_sum = process_memory_sum + sample_data
+        process_memory_value = process_memory_sum / len(memory_all_data[process_name])
         process_memory_pie_data = {'name': process_name,'value': round(process_memory_value, 3)}
         process_memory_pie_data_array.append(process_memory_pie_data)
-
+        system_memory_sum = system_memory_sum + process_memory_value
     process_memory_pie_data = {'name': 'Others', 'value':  round(MAX_MEMORY_SIZE - system_memory_sum, 3)}
     process_memory_pie_data_array.append(process_memory_pie_data)
     process_memory_pie_series = {'name': 'Process Memory (MB)', 'type':'pie', 'radius': '50%', 'data': sorted(process_memory_pie_data_array, key=lambda k:(k['value']), reverse=True)}
@@ -102,45 +148,33 @@ def process_charts_data():
     process_cpu_pie_data_array = []
     MAX_CPU=100
     system_cpu_sum = 0
-    process_cpu_map = {}
-
-    for ch_key in process_cpu_samples:
-        process_name = ch_key[0:ch_key.rfind('-')]
+    for process_name in cpu_all_data:
         process_cpu_sum = 0
-        for sample_data in process_cpu_samples[ch_key]:
-            process_cpu_sum = process_cpu_sum + sample_data[1]
-        process_cpu_value = process_cpu_sum / len(process_cpu_samples[ch_key])
-        if process_name not in process_cpu_map:
-            process_cpu_map[process_name] = process_cpu_value
-        else:
-            process_cpu_map[process_name] = process_cpu_map[process_name] + process_cpu_value
-    
-    for process_name in process_cpu_map:
-        process_cpu_value = round(process_cpu_map[process_name]/12,3)
-        system_cpu_sum = system_cpu_sum + process_cpu_value
-        process_cpu_pie_data = {'name': process_name,'value': process_cpu_value}
+        for sample_data in cpu_all_data[process_name]:
+            process_cpu_sum = process_cpu_sum + sample_data
+        process_cpu_value = process_cpu_sum / len(cpu_all_data[process_name])
+        process_cpu_pie_data = {'name': process_name,'value': round(process_cpu_value, 3)}
         process_cpu_pie_data_array.append(process_cpu_pie_data)
-
+        system_cpu_sum = system_cpu_sum + process_cpu_value
     process_cpu_pie_data = {'name': 'Idle', 'value':  round(MAX_CPU - system_cpu_sum,3)}
-    print(process_cpu_pie_data)
     process_cpu_pie_data_array.append(process_cpu_pie_data)
     process_cpu_pie_series = {'name': 'Process CPU (%)', 'type':'pie', 'radius': '50%', 'data': sorted(process_cpu_pie_data_array, key=lambda k:(k['value']), reverse=True)}
     process_cpu_pie_charts_data = {'title': 'CPU Pies','series':process_cpu_pie_series}
-    
+
     process_memory_series_array = []
     process_memory_legend_array = []
-    for ch_key in process_memory_samples:
-        process_memory_series = {'symbolSize': 5, 'type':'line', 'name': ch_key, 'data': sample_log_data(process_memory_samples[ch_key])}
+    for process_name in memory_all_data:
+        process_memory_series = {'symbolSize': 5, 'type':'line', 'name': process_name, 'data': sample_log_data(memory_all_data[process_name])}
         process_memory_series_array.append(process_memory_series)
-        process_memory_legend_array.append(ch_key)
+        process_memory_legend_array.append(process_name)
     process_memory_charts_data = {'title': 'Memory Lines','series':process_memory_series_array, 'legend': process_memory_legend_array}
 
     process_cpu_series_array = []
     process_cpu_legend_array = []
-    for ch_key in process_cpu_samples:
-        process_cpu_series = {'symbolSize': 5, 'type':'line', 'name': ch_key, 'data': sample_log_data(process_cpu_samples[ch_key])}
+    for process_name in cpu_all_data:
+        process_cpu_series = {'symbolSize': 5, 'type':'line', 'name': process_name, 'data': sample_log_data(cpu_all_data[process_name])}
         process_cpu_series_array.append(process_cpu_series)
-        process_cpu_legend_array.append(ch_key)
+        process_cpu_legend_array.append(process_name)
     process_cpu_charts_data = {'title': '%','series':process_cpu_series_array, 'legend': process_cpu_legend_array}
 
     charts_data_map = {
@@ -151,10 +185,12 @@ def process_charts_data():
     }
     return charts_data_map
 
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Error: input log file(*.log) not specified')
         sys.exit()
+    
 
     path = os.getcwd().replace("\\", "/")
     temp_path = '/templates/template.html'
@@ -171,11 +207,9 @@ if __name__ == '__main__':
     print("lines: ", len(log_lines))
     process_log_data()
 
-    charts_data = process_charts_data()
+    charts_data = process_all_charts_data()
+
     temp_out = temp.render(charts_data_map=charts_data)
-
-    
-
     full_path2 = path+'/reports'
     if not os.path.exists(full_path2):
         os.mkdir(full_path2)
